c161c246e1d1b694cb92189d08a57d39b3d2f452
jpcuzon <62604255+jpcuzon@users.noreply.github.com>
2020-11-07 00:23:20 +0000

    Test Commit

--- src/ca_gui/CA_GUI.java
+++ src/ca_gui/CA_GUI.java
@@ -14,8 +14,9 @@ public class CA_GUI {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        System.out.println("Test");
     }
     
 }
